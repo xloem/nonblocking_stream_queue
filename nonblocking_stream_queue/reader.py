bce8f60dec10e7fe129ed936c92a3b0a009d5607
John Doe <johndoe@example.com>
2022-10-25 11:21:07 -0400

    fix race conditions associated with daemon threads using streams

--- nonblocking_stream_queue/reader.py
+++ nonblocking_stream_queue/reader.py
@@ -1,5 +1,5 @@
 import sys, threading, time
-from six.moves import queue # python3 queue
+from six.moves import queue # python2/python3 queue
 
 class Reader:
     def __init__(self, stream, max_size=-1, lines=False, max_count=None, drop_timeout=None, transform_cb=None):
@@ -25,13 +25,14 @@ class Reader:
         self.drop_timeout = drop_timeout
         self.queue = queue.Queue(max_count or 0)
         self.transform_cb = transform_cb
+        self.parent_thread = threading.current_thread()
         self.thread = threading.Thread(target=self._pump)
-        self.thread.daemon = True # terminate with process
         self.condition = threading.Condition()
         self._is_pumping = True
         self.thread.start()
+
     def __del__(self):
-        self.stream.close()
+        self._is_pumping = False
         self.thread.join()
 
     def __iter__(self):
@@ -59,7 +60,7 @@ class Reader:
             return self.queue.qsize()
 
     def _pump(self):
-        while not self.stream.closed:
+        while self._is_pumping and not self.stream.closed and self.parent_thread.is_alive():
             data = self._read(self.max_size or -1)
             
             if data is None:
